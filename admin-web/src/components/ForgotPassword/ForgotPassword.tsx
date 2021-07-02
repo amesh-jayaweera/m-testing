7ca9dc529d0b4a53b878bc68c5b7fb72065f4f57
ameshmjayaweera <ameshmbjyw97@gmail.com>
2021-07-03 04:07:01 +0530

    forgot password completed

--- admin-web/src/components/ForgotPassword/ForgotPassword.tsx
+++ admin-web/src/components/ForgotPassword/ForgotPassword.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../../resources/images/logo_light.svg";
+import firebase from "firebase";
+import {ToastContainer} from "react-toastify";
+import {Failure, Success} from "../../util/Toasts";
 
 export function ForgotPassword() {
+
+    const [email,setEmail] = useState<string>();
+    const [emailReq, setEmailReq] = useState<boolean>(false);
+
+    function onSubmit() {
+        if(email) {
+            firebase.auth().sendPasswordResetEmail(email)
+                .then(() => {
+                    // Password reset email sent!
+                    Success("Password reset email sent!\nPlease check your mail box");
+                })
+                .catch((error) => {
+                    // const errorCode = error.code;
+                    const errorMessage = error.message;
+                    Failure(errorMessage);
+                });
+        }
+    }
 
     return (
         <div className="fix-menu">
@@ -10,7 +31,7 @@ export function ForgotPassword() {
                     <div className="row">
                         <div className="col-sm-12">
                             <div className="login-card card-block auth-body mr-auto ml-auto">
-                                <form className="md-float-material needs-validation" noValidate>
+                                <div className="md-float-material needs-validation">
                                     <div className="auth-box m-2">
                                         <img className="img-fluid" src={logo} alt="logo"/>
                                             <div className="row p-o m-0">
@@ -24,26 +45,38 @@ export function ForgotPassword() {
                                             </div>
                                             <div className="form-group">
                                                 <input type="email" className="form-control"
-                                                       placeholder="Your email address" required/>
-                                                    <small className="invalid-feedback text-left">Please enter your
+                                                       placeholder="Your email address" required
+                                                       onChange={(e) =>
+                                                            {
+                                                                setEmail(e.target.value);
+                                                                setEmailReq(!e.target.value);
+                                                            }
+                                                        }
+                                                />
+                                                {
+                                                    emailReq && <small className="invalid-feedback text-left">Please enter your
                                                         email address.</small>
+                                                }
                                             </div>
-
-
+                                            <div className="input-group text-center mt-4">
+                                                <a className="w-100" href="/login">Back to Login</a>
+                                            </div>
                                             <div className="row mt-2">
                                                 <div className="col-md-12">
-                                                    <button type="submit" className="btn btn-primary btn-block">Submit
+                                                    <button  className="btn btn-primary btn-block"
+                                                             onClick={() => {onSubmit();}}
+                                                    >Submit
                                                     </button>
                                                 </div>
                                             </div>
-
                                     </div>
-                                </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
+            <ToastContainer/>
         </div>
     )
 }
